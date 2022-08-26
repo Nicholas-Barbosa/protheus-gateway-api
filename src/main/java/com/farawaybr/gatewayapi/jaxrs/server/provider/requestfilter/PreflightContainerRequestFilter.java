435950a7797594f8144a8a678cf7fe42044c72e3
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2022-08-26 11:40:34 -0300

    added prematching to preflight requ

--- src/main/java/com/farawaybr/gatewayapi/jaxrs/server/provider/requestfilter/PreflightContainerRequestFilter.java
+++ src/main/java/com/farawaybr/gatewayapi/jaxrs/server/provider/requestfilter/PreflightContainerRequestFilter.java
@@ -6,11 +6,13 @@ import java.util.List;
 import jakarta.annotation.Priority;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
 @Priority(0)
+@PreMatching
 public class PreflightContainerRequestFilter implements ContainerRequestFilter {
 
 	@Override
